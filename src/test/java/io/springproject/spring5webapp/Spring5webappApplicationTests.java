ca3fa811df1b41f791079b1fb81122c98996ed3f
Munanga <jackslaighter@gmail.com>
2021-11-23 21:57:42 +0200

    first commit

---
+++ src/test/java/io/springproject/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springproject.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
